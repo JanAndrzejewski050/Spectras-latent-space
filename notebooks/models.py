332dd2660243afc74251223c6d7be59ecc76a45f
JanAndrzejewski050 <janandrzejewski050@gmail.com>
2026-06-20 18:33:46 +0200

    fix

--- notebooks/models.py
+++ notebooks/models.py
@@ -227,3 +227,339 @@ def vae_loss(preds, targets, mu, logvar, beta=0.1):
     rec = F.mse_loss(preds, targets)
     kl  = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
     return rec + beta * kl, rec, kl
+
+
+# ------- Molecula Model ----------
+# model
+class PositionalEmbedding(nn.Module):
+    def __init__(self, max_len, hidden_size):
+        super().__init__()
+        self.hidden_size = hidden_size
+        pe = torch.zeros(max_len, hidden_size)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, hidden_size, 2).float() * (-math.log(10000.0) / hidden_size))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        if x.dim() == 3:
+            B, T, _ = x.shape
+        elif x.dim() == 2:
+            B, T = x.shape
+        if T <= self.pe.size(0):
+            pe = self.pe[:T]  
+        else:
+            device = x.device
+            H = self.hidden_size
+            position = torch.arange(T, dtype=torch.float, device=device).unsqueeze(1)  
+            div_term = torch.exp(torch.arange(0, H, 2, device=device).float() * (-math.log(10000.0)/H))
+            pe = torch.zeros(T, H, device=device)
+            pe[:, 0::2] = torch.sin(position * div_term)
+            pe[:, 1::2] = torch.cos(position * div_term)
+        return pe.unsqueeze(0)  
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, hidden_size, num_heads):
+        super().__init__()
+        self.d = hidden_size // num_heads
+        self.num_heads = num_heads
+        self.W_q = nn.Linear(hidden_size, hidden_size, bias=False)
+        self.W_k = nn.Linear(hidden_size, hidden_size, bias=False)
+        self.W_v = nn.Linear(hidden_size, hidden_size, bias=False)
+        self.W_o = nn.Linear(hidden_size, hidden_size, bias=False)
+        self.norm1 = nn.LayerNorm(hidden_size)
+        self.ff = nn.Sequential(
+            nn.Linear(hidden_size, 2*hidden_size),
+            nn.GELU(),
+            nn.Dropout(p=0.1),
+            nn.Linear(2*hidden_size, hidden_size),
+            nn.Dropout(p=0.1)
+        )
+        self.norm2 = nn.LayerNorm(hidden_size)
+
+    def forward(self, q, k, v, pad_mask=None):   # [B, T, H]
+        # q,k,v: attention values, D: molecule distance matrix (selfies) [B, T, T], alpha: how much D should influence attention
+        B, T_q, H = q.shape
+        _, T_v, _ = v.shape
+        Q = self.W_q(q)     # [B, T, num_heads * H]
+        K = self.W_k(k)
+        V = self.W_v(v)
+        Q = Q.view(B, self.num_heads, T_q, self.d) # [B, A, T, H]
+        K = K.view(B, self.num_heads, T_v, self.d)
+        V = V.view(B, self.num_heads, T_v, self.d)
+
+        attn_logits = torch.einsum('baih,bajh->baij', Q, K)    # [B, A, T, H] @ [B, A, H, T] = [B, A, T, T]
+
+        if pad_mask is not None:
+            key_mask = pad_mask[:, None, None, :]  # [B,1,1,T_k]
+            attn_logits = attn_logits.masked_fill(~key_mask, float('-inf'))
+
+        attn = F.softmax(attn_logits / math.sqrt(self.d), dim=-1)
+
+        if pad_mask is not None:
+            query_mask = pad_mask[:, None, :, None]  # [B,1,T_q,1]
+            attn = attn * query_mask.float()
+
+        h = torch.einsum('baij,bajh->baih',attn, V)  # [B, A, T, H]
+        h = h.view(B, T_q, H)  # [B, T, A*H]
+        h = self.W_o(h)     # [B, T, H]
+        
+        h = q + self.W_o(h)     # [B, T, H]
+        h = h + self.ff(self.norm2(h))
+        h = self.norm1(h)
+        return h, attn
+
+class MultiSlotPooling(nn.Module):
+    def __init__(self, hidden_size, num_slots):
+        super().__init__()
+        self.queries = nn.Parameter(torch.randn(num_slots, hidden_size))
+        self.W_k = nn.Linear(hidden_size, hidden_size, bias=False)
+        self.W_v = nn.Linear(hidden_size, hidden_size, bias=False)
+
+    def forward(self, h, mask):
+        # h: [B, T, D]
+        # mask: [B, T]
+        k = self.W_k(h)
+        v = self.W_v(h)
+        attn = torch.einsum("kd,btd->bkt", self.queries, k)
+        attn = attn.masked_fill(~mask[:, None, :], -1e9)
+        attn = F.softmax(attn, dim=-1)
+        slots = torch.einsum("bkt,btd->bkd", attn, v)
+        return slots
+
+
+class VaeTransformer(nn.Module):
+    def __init__(self, vocab_size, hidden_size, latent_size, max_len, attn_heads=8, num_slots=8, encoder_layers=1, decoder_layers=1):
+        super().__init__()
+        self.hidden_size = hidden_size
+        self.num_slots = num_slots
+        self.embedding = nn.Embedding(vocab_size, hidden_size)
+        self.pos_encoder = PositionalEmbedding(max_len, hidden_size)
+        self.conv_pos = nn.Conv1d(hidden_size, hidden_size, kernel_size=3, padding=1, groups=hidden_size)
+        
+        # Encoder
+        self.pos_block = MultiHeadAttention(hidden_size, attn_heads)
+        self.encoder_blocks = nn.ModuleList([MultiHeadAttention(hidden_size, attn_heads) for _ in range(encoder_layers)])
+        self.pool = MultiSlotPooling(hidden_size, num_slots=num_slots)
+        #self.slot_pos_encoder = nn.Parameter(torch.randn(1, num_slots, hidden_size))
+        self.slots_mix = MultiHeadAttention(hidden_size, num_heads=num_slots)
+
+        self.slot_gamma = nn.Parameter(torch.ones(1, num_slots, hidden_size))
+        self.slot_beta = nn.Parameter(torch.zeros(1, num_slots, hidden_size))
+        #nn.init.orthogonal_(self.slot_beta[0])
+
+        # VAE heads
+        self.slot_mu = nn.Linear(hidden_size, latent_size)
+        self.slot_logvar = nn.Linear(hidden_size, latent_size)  
+        
+        self.slot_compress_mu = nn.Linear(hidden_size, latent_size // num_slots)
+        self.slot_compress_logvar = nn.Linear(hidden_size, latent_size // num_slots)
+
+        self.fc_mu = nn.Linear(hidden_size, latent_size)
+        self.fc_logvar = nn.Linear(hidden_size, latent_size)
+        
+        # pooling in latent space with uncertanty
+        self.latent_query = nn.Parameter(torch.randn(1, 1, latent_size))
+        self.latent_key = nn.Linear(latent_size, latent_size)
+
+        # Decoder
+        self.max_len = max_len
+        self.z_to_slot = nn.Linear(hidden_size // num_slots, hidden_size)
+        #self.slot_pos_decoder = nn.Parameter(torch.randn(1, num_slots, hidden_size))
+        self.decoder_embed = nn.Embedding(vocab_size, hidden_size)
+
+        self.decoder_pos = PositionalEmbedding(max_len, hidden_size)
+
+        self.z_to_memory = nn.Linear(latent_size, hidden_size)
+        self.slots_to_memory = nn.Linear(latent_size // num_slots, hidden_size)
+
+        self.decoder_layer = nn.TransformerDecoderLayer(
+            d_model=hidden_size,
+            nhead=attn_heads,
+            dim_feedforward=2 * hidden_size,
+            batch_first=True
+        )
+
+        self.decoder_transformer = nn.TransformerDecoder(
+            self.decoder_layer,
+            num_layers=decoder_layers
+        )
+        # Output head
+        self.fc_output = nn.Linear(hidden_size, vocab_size)
+        
+        # Initialize weights
+        self._init_weights()
+    
+    def _init_weights(self):
+        for p in self.parameters():
+            if p.dim() > 1:
+                nn.init.xavier_uniform_(p)
+
+    def causal_mask(self, T, device):
+        return torch.triu(
+            torch.ones(T, T, device=device),
+            diagonal=1
+        ).bool()
+    
+    def encode(self, x, D, mode=None):  
+        B, _ = x.shape
+        h = self.embedding(x)    # [B, T, H]
+        fourier_pos_encoding = self.pos_encoder(x)
+        # invalid_tokens = (D == -1).all(dim=-1)   # [B, T]
+        # fourier_pos_encoding = fourier_pos_encoding.masked_fill(invalid_tokens[:, :, None],0.0)
+        h = h + fourier_pos_encoding
+
+        # D_mix = 1.0 / (1.0 + torch.clamp(D.float(), min=0))
+        # D_mix = D_mix.masked_fill(D == -1, 0.0) 
+        # h = torch.einsum("bij,bjh->bih", D_mix, h)
+
+        mask = (x != 0)
+        
+        for block in self.encoder_blocks:
+            h, attn_matrix = block(h, h, h, pad_mask=mask)
+            
+        h = h.masked_fill(~mask[:, :, None], 0.0)
+
+        # h = h.sum(dim=1) / mask.sum()
+        # mu = self.fc_mu(h)
+        # logvar = self.fc_logvar(h)
+        slots = self.pool(h, mask)
+        slots = F.layer_norm(slots, slots.shape[-1:])
+        #slots = slots * self.slot_gamma + self.slot_beta
+
+        B, K, Z = slots.shape
+
+        slots_mu = self.slot_mu(slots)
+        slots_logvar = self.slot_logvar(slots)
+
+        # Latent pool from slots with uncertanty
+        q = F.normalize(self.latent_query.expand(B, 1, Z), dim=-1)
+        k = F.normalize(self.latent_key(slots_mu), dim=-1)
+
+        logits = torch.einsum("bqz,bkz->bqk", q, k)
+
+        confidence = -torch.logsumexp(slots_logvar, dim=-1).unsqueeze(1) #-slots_logvar.mean(dim=-1).unsqueeze(1)
+        confidence_scale = 0.5
+
+        logits = logits + confidence_scale * confidence
+        attn = torch.softmax(logits / 0.5, dim=-1)
+
+        mu = torch.einsum("bqk,bkz->bqz", attn, slots_mu).squeeze(1)
+
+        var = torch.exp(slots_logvar)
+        var_agg = torch.einsum("bqk,bkz->bqz", attn, var).squeeze(1)
+        logvar = torch.log(var_agg + 1e-8)
+        
+        if mode == "test":
+            return mu, logvar, attn_matrix
+        else:
+            return mu, logvar
+
+    def reparameterize(self, mu, logvar):
+        if self.training:
+            std = torch.exp(0.5 * logvar)
+            eps = torch.randn_like(std)
+            return mu + eps * std
+        else:
+            return mu
+
+    def decode(self, z, x_in=None, max_len=80, start_id=1, eos_id=2):
+        B = z.size(0)
+        device = z.device
+
+        memory = self.z_to_memory(z).unsqueeze(1)
+
+        if x_in is not None:
+            x_emb = self.decoder_embed(x_in)
+            x_emb = x_emb + self.decoder_pos(x_emb)
+
+            T = x_emb.size(1)
+            tgt_mask = self.causal_mask(T, device)
+
+            h = self.decoder_transformer(
+                tgt=x_emb,
+                memory=memory,
+                tgt_mask=tgt_mask
+            )
+
+            logits = self.fc_output(h)   
+            return logits
+
+        else:
+            tokens = torch.full((B, 1), start_id, dtype=torch.long, device=device)
+            finished = torch.zeros(B, dtype=torch.bool, device=device)
+            max_len = self.max_len * 2
+
+            for _ in range(max_len):
+                x_emb = self.decoder_embed(tokens)
+                x_emb = x_emb + self.decoder_pos(x_emb)
+
+                T = tokens.size(1)
+                tgt_mask = self.causal_mask(T, device)
+
+                h = self.decoder_transformer(
+                    tgt=x_emb,
+                    memory=memory,
+                    tgt_mask=tgt_mask
+                )
+
+                logits_step = self.fc_output(h[:, -1])  # [B, V]
+
+                next_token = torch.argmax(logits_step, dim=-1, keepdim=True)
+
+                next_token = torch.where(
+                    finished.unsqueeze(1),
+                    torch.full_like(next_token, eos_id),
+                    next_token
+                )
+
+                tokens = torch.cat([tokens, next_token], dim=1)
+
+                finished |= (next_token.squeeze(1) == eos_id)
+
+                if finished.all():
+                    break
+
+            return tokens
+            
+    def forward(self, x, D, mode='eval'):
+        mu, logvar = self.encode(x, D)
+        z = self.reparameterize(mu, logvar)
+
+        if mode == 'train':
+            x_in = x[:, :-1]
+
+            logits = self.decode(z, x_in=x_in)
+
+            return logits, mu, logvar, z
+    
+        if mode == "eval":
+            tokens = self.decode(z, x_in=None)
+
+            return tokens, mu, logvar, z
+        
+        if mode == "test":
+            mu, logvar, attn_matrix = self.encode(x, D, mode="test")
+            z = self.reparameterize(mu, logvar)
+
+            tokens = self.decode(z, x_in=None)
+
+            return tokens, mu, logvar, attn_matrix, z
+
+
+def vae_loss(logits, targets, mu, logvar, beta=0.01, pad_id=0):
+    B, T, V = logits.shape
+
+    logits = logits.reshape(-1, V)
+    targets = targets.reshape(-1)
+
+    rec_loss = F.cross_entropy(
+        logits,
+        targets,
+        ignore_index=pad_id
+    )
+
+    kl = -0.5 * torch.mean(1 + logvar - mu.pow(2) - logvar.exp())
+
+    return rec_loss + beta * kl, rec_loss, kl
